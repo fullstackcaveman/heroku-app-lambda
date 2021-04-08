1c32c13a6fa761772bf9b8343b0d4c7986a154f0
Chris Burton <chris@fullstackcaveman.com>
2021-04-08 10:57:48 -0600

    stuff

---
+++ index.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', (_req, res) => {
+	res.json({ data: 'Api is accounted for' });
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port: ${port}`);
+});
+
+// console.log('Its aliiiiveeee!');
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.env.USERNAME);
+// console.log(process.env.PORT);
